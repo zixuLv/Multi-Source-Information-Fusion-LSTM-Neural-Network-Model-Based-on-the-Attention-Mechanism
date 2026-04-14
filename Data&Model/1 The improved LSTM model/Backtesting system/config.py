6449fc6dd0db7a5585f9d64147c4723207690c52
zixuLv <lvzixu666@gmail.com>
2026-04-14 13:14:47 +0800

    Add files via upload

---
+++ Data&Model/1 The improved LSTM model/Backtesting system/config.py
@@ -0,0 +1,19 @@
+# config.py
+
+# Strategy parameters
+
+# Data file paths
+data_paths = {
+    "suoyin": r"data\suoyin.csv",
+    "btc_data": r"data\btc.csv"
+}
+
+# Strategy file directory
+strategy_dir = r"strategies"
+
+# Result output paths
+result_csv = r"results"
+result_plot_dir = r"results"
+
+# Other configuration
+random_seed = 123
