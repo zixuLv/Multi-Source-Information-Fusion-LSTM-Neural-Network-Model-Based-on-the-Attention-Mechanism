ca9dbe38391dec02ae7f69f8140f89caecb62f86
zixuLv <lvzixu666@gmail.com>
2026-04-14 13:13:47 +0800

    Add files via upload

---
+++ Data&Model/1 The improved LSTM model/ML/ml/config.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*- 
+import os
+
+beishu=1
+model_list=["SupportVectorMachine","RandomForest","DecisionTree","Multi_LayerPerceptron","KNN","GBDT","Ada"]#"BayesClassifier",""QDA",Logistic_Regression","LinearDiscriminant"]
+
+canshu = {'batch_size_num': 32, 
+          'sequence_length_num': 18,
+          'input_size_1': 4, 
+          'input_size_2': 23,
+          'input_size_3': 8, 
+          'input_size_4': 20}
